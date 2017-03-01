7804f42e0a121755898013563e87fa8ba98cfec7
EyJanChen <EyJanChen@users.noreply.github.com>
2017-03-01 19:13:25 +0800

    Update FruitBox.js
    水果发光实现

--- Fruit/src/boxs/FruitBox.js
+++ Fruit/src/boxs/FruitBox.js
@@ -2,31 +2,63 @@ var FruitBox = (function(_super) {
     function FruitBox(fruitInfo) {
         FruitBox.super(this);
 
-        this.fruitInfo = fruitInfo;
+        this.name = fruitInfo.fruitName;
+        this.multiple = fruitInfo.multiple;
 
+        this.showType = FruitBox.SHOW_TYPE.ONLY_FRUIT;
         this.init();
     }
 
     Laya.class(FruitBox, "FruitBox", _super);
 
     FruitBox.prototype.init = function () {
-        var fruitName = this.fruitInfo.fruitName;
-        var multiple = this.fruitInfo.multiple;
-
         this.bigIcon.skin = "";
         this.middleIcon.skin = "";
         this.numLab.text = "";
         this.numLab.font = "brown";
 
-        if (multiple > 1) {
-            this.middleIcon.skin = App.uiManager.getMiddleFruitIconPath(fruitName);
-            this.numLab.text = "X" + multiple;
+        if (this.multiple > 1) {
+            this.numLab.text = "X" + this.multiple;
+            this.showType = FruitBox.SHOW_TYPE.HAVE_MULTIPLE;
         }
         else {
-            this.bigIcon.skin = App.uiManager.getBigFruitIconPath(fruitName);
+            this.showType = FruitBox.SHOW_TYPE.ONLY_FRUIT;
+        }
+
+        this.setFruitUnLight();
+    };
+
+    FruitBox.prototype.setFruitLighting = function () {
+        switch (this.showType) {
+            case FruitBox.SHOW_TYPE.HAVE_MULTIPLE: {
+                this.middleIcon.skin = App.uiManager.getMiddleGlowFruitIconPath(this.name);
+                break;
+            }
+            case FruitBox.SHOW_TYPE.ONLY_FRUIT: {
+                this.bigIcon.skin = App.uiManager.getBigGlowFruitIconPath(this.name);
+                break;
+            }
         }
     };
 
+    FruitBox.prototype.setFruitUnLight = function () {
+        switch (this.showType) {
+            case FruitBox.SHOW_TYPE.HAVE_MULTIPLE: {
+                this.middleIcon.skin = App.uiManager.getMiddleFruitIconPath(this.name);
+                break;
+            }
+            case FruitBox.SHOW_TYPE.ONLY_FRUIT: {
+                this.bigIcon.skin = App.uiManager.getBigFruitIconPath(this.name);
+                break;
+            }
+        }
+    };
+
+
+    FruitBox.SHOW_TYPE = {
+        ONLY_FRUIT: 1,
+        HAVE_MULTIPLE: 2
+    };
 
     return FruitBox;
 }(FruitBoxUI));
